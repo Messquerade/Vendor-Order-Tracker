3e439d5bb84654f49aca2648e7ef74fedb17bb4b
Messquerade <annac.klingberg@gmail.com>
2021-10-01 13:47:01 -0700

    add order property tests, pass

--- VOT.Tests/ModelTests/OrderTests.cs
+++ VOT.Tests/ModelTests/OrderTests.cs
@@ -19,6 +19,32 @@ namespace VOT.Tests
       Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetTitle_ReturnTitle_String()
+    {
+      Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
+      Assert.AreEqual("Order 1", newOrder.Title);
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnDescription_String()
+    {
+      Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
+      Assert.AreEqual("20 loaves of bread, 50 pastries", newOrder.Description);
+    }
+    [TestMethod]
+    public void GetPrice_ReturnPrice_Double()
+    {
+      Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
+      Assert.AreEqual(154.00, newOrder.Price);
+    }
+    [TestMethod]
+    public void GetDate_ReturnDate_String()
+    {
+      Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
+      Assert.AreEqual("2021-09-25", newOrder.Date);
+    }
   
   }
 }
