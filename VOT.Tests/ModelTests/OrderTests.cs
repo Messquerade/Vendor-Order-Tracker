c8baa116d05e014847c728167d5d3097cd00057c
Messquerade <annac.klingberg@gmail.com>
2021-10-01 13:24:47 -0700

    add Order properties and constructor

--- VOT.Tests/ModelTests/OrderTests.cs
+++ VOT.Tests/ModelTests/OrderTests.cs
@@ -6,8 +6,19 @@ using System;
 namespace VOT.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests: IDisposable
   {
-    
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
+    [TestMethod]
+    public void OrderConstructor_CheckTypeOrder_Order()
+    {
+      Order newOrder = new Order("Order 1", "20 loaves of bread, 50 pastries", 154.00, "2021-09-25");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+  
   }
 }
--- VOT/Models/Order.cs
+++ VOT/Models/Order.cs
@@ -1,7 +1,31 @@
+using System.Collections.Generic;
+
 namespace VOT.Models
 {
   public class Order
   {
-    
+    public string Title {get; set;}
+    public string Description {get; set;}
+    public double Price {get; set;}
+    public string Date {get; set;}
+    public int Id {get;}
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order(string title, string description, double price, string date)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      Date = date;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+
   }
 }
