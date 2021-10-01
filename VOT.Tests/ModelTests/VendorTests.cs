2af4e23c25a40a54b2080d33c8bada53a849a1ad
Messquerade <annac.klingberg@gmail.com>
2021-10-01 10:06:28 -0700

    add Vendor class constructor

--- VOT.Tests/ModelTests/VendorTests.cs
+++ VOT.Tests/ModelTests/VendorTests.cs
@@ -8,6 +8,11 @@ namespace VOT.Tests
   [TestClass]
   public class VendorTests
   {
-    
+    [TestMethod]
+    public void VendorConstructor_TypeIsVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Larry's Cafe", "A small cafe with weekly orders.");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
   }
 }
--- VOT/Models/Vendor.cs
+++ VOT/Models/Vendor.cs
@@ -2,6 +2,13 @@ namespace VOT.Models
 {
   public class Vendor
   {
-    
+    public string Name {get; set;}
+    public string Description {get; set;}
+
+    public Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+    }
   }
 }
