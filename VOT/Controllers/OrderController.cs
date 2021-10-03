8b8d320af716e5d167cd80bb17c8ae6060d2850a
Messquerade <annac.klingberg@gmail.com>
2021-10-03 09:19:34 -0700

    add order index and new views

--- VOT/Controllers/OrderController.cs
+++ VOT/Controllers/OrderController.cs
@@ -7,12 +7,21 @@ namespace VOT.Controllers
 {
   public class OrderController : Controller
   {
-    [HttpGet("/vendor/{Id}/order")]
-    public ActionResult Index(int vendorId)
+    [HttpGet("/vendor/{Id}/order/")]
+    public ActionResult Index(int Id)
     {
-      Vendor currentVendor = Vendor.Find(vendorId);
-      List<Order> vendorOrders = currentVendor.Orders;
-      return View(vendorOrders);
+      Vendor currentVendor = Vendor.Find(Id);
+      // Dictionary<string, List<Order>> model = new Dictionary<Vendor, List<Order>> {{currentVendor.Name, currentVendor.Orders}}
+      return View(currentVendor);
     }
+
+    [HttpGet("/vendor/{Id}/order/new")]
+    public ActionResult New(int Id)
+    {
+      Vendor currentVendor = Vendor.Find(Id);
+      return View(currentVendor);
+    }
+
+
   }
 }
